c39b7f3b851691ebfe913db23af4ce6e5c3aadea
lavelee <lavelee15@gmail.com>
2016-11-08 16:29:08 +0900

    jquery css styling

--- lecture/161108/jquery2/js/js-main.js
+++ lecture/161108/jquery2/js/js-main.js
@@ -14,3 +14,23 @@ jQuery(function(this.$){
 (function($){
   내용내용
 })(this.jQuery);
+
+
+
+
+
+var fds = (function(global, $) {
+  'use strict';
+  return {$: jQuery}
+})(this, this.jquery.noConflict(ture));
+// 비공개 멤버인 jquery에 접근 가능한 유일한 방법 특권부여
+// noConflict(true) 때문에 $도, jQuery 키워드도 사라졌다.
+//  jQuery === undefined
+//  fds.$ === jQuery . 이렇게만 접근 가능해진다
+
+
+(function(fds) {
+  'use strict';
+  var jQuery_version = fds.$().jqeury;
+  console.log('jQuery_version:', jQuery_version);
+})(this, fds);
