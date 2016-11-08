6c05eadfc6569819f49d0d75ecf7377ea8782ebb
lavelee <lavelee15@gmail.com>
2016-11-08 19:25:15 +0900

    jquery

--- lecture/161108/jquery_css_styling/resource/js/js-main.js
+++ lecture/161108/jquery_css_styling/resource/js/js-main.js
@@ -1,2 +1,13 @@
 var $p01 = $('.post-01')
 var $p0102 = $('.post-01, .post-02')
+
+
+//native
+var brand = document.querySelector('.brand');
+brand.style.color = '#6500ea';
+
+//jquery
+var $brand = $('.brand');
+$brand.css('color','#6500ea');
+
+$brand.css('font-size','3rem');
