f64fdba36e49a957b3ad5725ad2eb5e87a3c7935
lavelee <lavelee15@gmail.com>
2016-11-24 17:46:59 +0900

    install gulp

---
+++ lecture/161124/gulp_project/server.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+console.log("start");
+
+gulp.task('default', function() {
+  console.log("gulp initiated");
+});
